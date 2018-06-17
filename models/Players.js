91902e7d55219ee06ca53d3e58b4c8cb13f2651c
Mackenzie Gray <mackenzie.gray13@ncf.edu>
2018-06-17 13:23:13 -0400

    Prettifies, add images and styling.

--- models/Players.js
+++ models/Players.js
@@ -6,7 +6,21 @@ var PlayersSchema = new Schema({
     name: String,
     url: String,
     score: Number,
-    games_played: Number
+    bling: [String],
+    active: Boolean,
+    img_path: String,
+    stats: {
+        'FG%': Number,
+        PER: Number,
+        TRB: Number,
+        AST: Number,
+        'eFG%': Number,
+        PTS: Number,
+        'FT%': Number,
+        '3PT%': Number,
+        WS: Number,
+        G: Number
+    }
 });
 
 module.exports = mongoose.model('Players', PlayersSchema, 'players');
